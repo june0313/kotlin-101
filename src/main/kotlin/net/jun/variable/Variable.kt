be292fee0dd3d4ddc6f54b6594ca4cc28bcb6caa
Young Jun Park <june0313@gmail.com>
2017-12-03 23:59:08 +0900

    val, var study

---
+++ src/main/kotlin/net/jun/variable/Variable.kt
@@ -0,0 +1,15 @@
+package net.jun.variable
+
+val one: Int = 1
+val one1 = 1
+
+fun main(args: Array<String>) {
+    print(one)
+    print(one1)
+    println()
+
+    var two = 2
+    print(two)
+    two = 22
+    print(two)
+}
